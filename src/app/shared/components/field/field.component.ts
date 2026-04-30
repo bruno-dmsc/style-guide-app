74eead0139b1eb7db4af2a6d77fa5e0aa62573d1
Bruno iBR <bruno.damasceno@ibrsoft.com.br>
2026-04-30 09:17:35 -0300

    Alteração dos cards no app.HTML para componentes

---
+++ src/app/shared/components/field/field.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-field',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './field.component.html'
+})
+export class FieldComponent {
+  @Input() label: string = '';
+  @Input() required: boolean = false;
+  @Input() placeholder: string = '';
+  @Input() errorMessage: string = '';
+  @Input() helperText: string = '';
+}
